d0d77eafafae1b26d7c2f9b4ec7cab181d93b966
karabalta75 <saifullo.adi@gmail.com>
2020-06-24 21:06:45 +0000

    added new regions

--- test/apps.tf
+++ test/apps.tf
@@ -11,10 +11,10 @@ module "app1" {
   private_cidr_block3 = "10.0.3.0/24"
 }
 
-module "ohio" {
+module "singapore" {
   source = "./module"
   
-  region = "us-east-2"
+  region = "ap-southeast-1"
   cidr_block = "10.0.0.0/16"
   public_cidr_block1 = "10.0.101.0/24"
   public_cidr_block2 = "10.0.102.0/24"
@@ -28,6 +28,32 @@ module "oregon" {
   source = "./module"
   
   region = "us-west-2"
+  cidr_block = "10.0.0.0/16"
+  public_cidr_block1 = "10.0.101.0/24"
+  public_cidr_block2 = "10.0.102.0/24"
+  public_cidr_block3 = "10.0.103.0/24"
+  private_cidr_block1 = "10.0.1.0/24"
+  private_cidr_block2 = "10.0.2.0/24"
+  private_cidr_block3 = "10.0.3.0/24"
+}
+
+module "frankfurt" {
+  source = "./module"
+  
+  region = "eu-central-1"
+  cidr_block = "10.0.0.0/16"
+  public_cidr_block1 = "10.0.101.0/24"
+  public_cidr_block2 = "10.0.102.0/24"
+  public_cidr_block3 = "10.0.103.0/24"
+  private_cidr_block1 = "10.0.1.0/24"
+  private_cidr_block2 = "10.0.2.0/24"
+  private_cidr_block3 = "10.0.3.0/24"
+}
+
+module "sao_paolo" {
+  source = "./module"
+  
+  region = "sa-east-1"
   cidr_block = "10.0.0.0/16"
   public_cidr_block1 = "10.0.101.0/24"
   public_cidr_block2 = "10.0.102.0/24"
